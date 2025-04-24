188f2f2f568f396e5de463b20cbe8bed0534fc39
AAJJANE <jane.a.arshinova@gmail.com>
2025-04-24 19:39:09 +0300

    added local api server

--- ai.py
+++ ai.py
@@ -1,57 +1,25 @@
 import re
-import multiprocessing
-from azure.ai.inference import ChatCompletionsClient
-from azure.ai.inference.models import UserMessage
-from azure.core.credentials import AzureKeyCredential
-from config import DEEPSEEK_API_KEY
+import requests
+import json
 
-
-def _make_request(incomes, expenses, result_queue):
-    try:
-        endpoint = "https://models.github.ai/inference"
-        model_name = "deepseek/DeepSeek-R1"
-        token = DEEPSEEK_API_KEY
-
-        client = ChatCompletionsClient(
-            endpoint=endpoint,
-            credential=AzureKeyCredential(token),
-        )
-
-        response = client.complete(
-            messages=[
-                UserMessage(f"""Write a short review(up to 3 sentences for a user,
-                                whose incomes equal float({incomes})
-                                and expenses equal float({expenses}).
-                                Your response must be addressed to the user.
-                                Do not use any currencies in your response""")
-            ],
-            max_tokens=1000,
-            model=model_name,
-        )
-
-        cleaned = re.sub(r'<think>.*?</think>',
-                         '', response.choices[0].message.content,
-                         flags=re.DOTALL).strip()
-        result_queue.put(cleaned)
-    except Exception as e:
-        result_queue.put(f"Error: {e}")
+from config import MODEL, API_URL
 
 
 def ai_review(incomes, expenses):
-    result_queue = multiprocessing.Queue()
-    process = multiprocessing.Process(target=_make_request, args=(incomes, expenses, result_queue))
-    process.start()
-    process.join(timeout=60)
+    prompt = f"""Write a short review(up to 3 sentences for a user,
+                                whose incomes equal float({incomes})
+                                and expenses equal float({expenses}).
+                                Your response must be addressed to the user.
+                                Do not use any currencies in your response!"""
 
-    if process.is_alive():
-        process.terminate()
-        process.join()
-        return "Unfortunately the AI assistant is too busy to analyze your accounts :("
+    payload = {
+        "model": MODEL,
+        "prompt": prompt,
+        "stream": False,
+        "Content-Type": "application/json",
+    }
 
-    return result_queue.get()\
-        if not result_queue.empty() \
-        else "Unfortunately the AI assistant is too busy to analyze your accounts :("
-
-
-if __name__ == "__main__":
-    print(ai_review(1111, 777))
+    response = requests.post(API_URL, json=payload)
+    json_response = json.loads(response.text)
+    cleaned_response = re.sub(r'<think>.*?</think>', '', json_response['response'], flags=re.DOTALL)
+    return cleaned_response.strip()
--- config.py
+++ config.py
@@ -1,2 +1,3 @@
-DEEPSEEK_API_KEY="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"
+API_URL = "http://127.0.0.1:11434/api/generate"
+MODEL = "deepseek-r1:7b"
 COLOR_URL_BASE="https://x-colors.yurace.pro/api/random"
